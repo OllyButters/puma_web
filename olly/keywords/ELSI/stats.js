0f66a3216fdbe4ba05501e8973e49a422b06c067
Azure pipeline <you@example.com>
2023-11-21 01:43:30 +0000

    Automatically updating web files for olly

--- olly/keywords/ELSI/stats.js
+++ olly/keywords/ELSI/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',0],['2016',0],['2017',1],]);var citations =([['Year', 'Number of Citations'],['2014',143],['2015',0],['2016',0],['2017',19],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',0],['2016',0],['2017',1],]);var citations =([['Year', 'Number of Citations'],['2014',144],['2015',0],['2016',0],['2017',20],]);
