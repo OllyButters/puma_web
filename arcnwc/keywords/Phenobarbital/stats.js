79bc73b7411a050d338f6a4bbaacbc592d0e4890
Azure pipeline <you@example.com>
2024-02-13 17:11:56 +0000

    Automatically updating web files for arcnwc

--- arcnwc/keywords/Phenobarbital/stats.js
+++ arcnwc/keywords/Phenobarbital/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2017',1],['2018',3],]);var citations =([['Year', 'Number of Citations'],['2017',0],['2018',29],]);
+var papers =([['Year', 'Number of papers'],['2017',1],['2018',2],['2019',0],['2020',1],]);var citations =([['Year', 'Number of Citations'],['2017',0],['2018',18],['2019',0],['2020',19],]);
