3e72d00b447a9b378adae15d261b3590413f6db4
Azure pipeline <you@example.com>
2024-05-05 01:39:23 +0000

    Automatically updating web files for olly

--- olly/keywords/Cataclysmic variables/stats.js
+++ olly/keywords/Cataclysmic variables/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2008',1],['2009',0],['2010',1],]);var citations =([['Year', 'Number of Citations'],['2008',0],['2009',0],['2010',0],]);
+var papers =([['Year', 'Number of papers'],['2008',1],['2009',0],['2010',1],]);var citations =([['Year', 'Number of Citations'],['2008',30],['2009',0],['2010',13],]);
