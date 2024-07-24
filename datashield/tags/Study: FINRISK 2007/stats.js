e6398a5aaa428a20c2798baf9e60df6dbb142646
Azure pipeline <you@example.com>
2024-07-24 10:02:14 +0000

    Automatically updating web files for datashield

--- datashield/tags/Study: FINRISK 2007/stats.js
+++ datashield/tags/Study: FINRISK 2007/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2013',108],['2014',169],]);
+var papers =([['Year', 'Number of papers'],['2013',1],['2014',2],]);var citations =([['Year', 'Number of Citations'],['2013',108],['2014',605],]);
