907b099c467ad07bf7e8e5c4a4fd2bf2c988ea21
Azure pipeline <you@example.com>
2024-07-28 01:42:10 +0000

    Automatically updating web files for arcnwc

--- arcnwc/tags/CHI/stats.js
+++ arcnwc/tags/CHI/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2020',3],['2021',9],['2022',9],['2023',11],]);var citations =([['Year', 'Number of Citations'],['2020',26],['2021',54],['2022',43],['2023',37],]);
+var papers =([['Year', 'Number of papers'],['2020',3],['2021',9],['2022',9],['2023',11],['2024',1],]);var citations =([['Year', 'Number of Citations'],['2020',26],['2021',54],['2022',43],['2023',37],['2024',0],]);
