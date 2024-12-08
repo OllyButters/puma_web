a2c2c12063eed1bf10e04365a3837885606ddda3
Azure pipeline <you@example.com>
2024-12-08 01:37:44 +0000

    Automatically updating web files for arcnwc

--- arcnwc/mesh/Nursing Homes/stats.js
+++ arcnwc/mesh/Nursing Homes/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2020',1],['2021',0],['2022',3],]);var citations =([['Year', 'Number of Citations'],['2020',20],['2021',0],['2022',41],]);
+var papers =([['Year', 'Number of papers'],['2020',1],['2021',0],['2022',3],['2023',0],['2024',1],]);var citations =([['Year', 'Number of Citations'],['2020',20],['2021',0],['2022',41],['2023',0],['2024',0],]);
