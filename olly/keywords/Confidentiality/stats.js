1c37aef4beabf486da7fcffeed95449b657f9822
Azure pipeline <you@example.com>
2024-12-22 01:39:36 +0000

    Automatically updating web files for olly

--- olly/keywords/Confidentiality/stats.js
+++ olly/keywords/Confidentiality/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',2],]);var citations =([['Year', 'Number of Citations'],['2014',366],]);
+var papers =([['Year', 'Number of papers'],['2014',2],]);var citations =([['Year', 'Number of Citations'],['2014',370],]);
