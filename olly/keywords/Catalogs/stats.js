fd7de49b50a29832373704977cd0c80f7e4c685a
Azure pipeline <you@example.com>
2025-05-04 01:42:32 +0000

    Automatically updating web files for olly

--- olly/keywords/Catalogs/stats.js
+++ olly/keywords/Catalogs/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2010',1],]);var citations =([['Year', 'Number of Citations'],['2010',295],]);
+var papers =([['Year', 'Number of papers'],['2010',1],]);var citations =([['Year', 'Number of Citations'],['2010',296],]);
