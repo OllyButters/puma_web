fd26f85f667f33623d1e449f95bb650155796d9b
Azure pipeline <you@example.com>
2025-06-29 01:43:06 +0000

    Automatically updating web files for arcnwc

--- arcnwc/keywords/Pregnancy Complications/stats.js
+++ arcnwc/keywords/Pregnancy Complications/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],]);var citations =([['Year', 'Number of Citations'],['2016',242],['2017',61],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],]);var citations =([['Year', 'Number of Citations'],['2016',245],['2017',62],]);
--- arcnwc/mesh/Artificial Intelligence/stats.js
+++ arcnwc/mesh/Artificial Intelligence/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2022',1],['2023',0],['2024',1],]);var citations =([['Year', 'Number of Citations'],['2022',57],['2023',0],['2024',14],]);
+var papers =([['Year', 'Number of papers'],['2022',1],['2023',0],['2024',1],]);var citations =([['Year', 'Number of Citations'],['2022',58],['2023',0],['2024',15],]);
