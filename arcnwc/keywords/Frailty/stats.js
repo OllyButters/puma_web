4c5b9ea739e25efea19fae53767e2eb88e6c3b31
Azure pipeline <you@example.com>
2025-11-16 01:42:09 +0000

    Automatically updating web files for arcnwc

--- arcnwc/keywords/Frailty/stats.js
+++ arcnwc/keywords/Frailty/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2020',2],['2021',1],['2022',1],['2023',3],]);var citations =([['Year', 'Number of Citations'],['2020',22],['2021',5],['2022',28],['2023',0],]);
+var papers =([['Year', 'Number of papers'],['2020',2],['2021',1],['2022',1],['2023',3],]);var citations =([['Year', 'Number of Citations'],['2020',24],['2021',5],['2022',28],['2023',0],]);
--- arcnwc/keywords/Oxcarbazepine/stats.js
+++ arcnwc/keywords/Oxcarbazepine/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2017',1],['2018',2],['2019',0],['2020',1],]);var citations =([['Year', 'Number of Citations'],['2017',0],['2018',248],['2019',0],['2020',8],]);
+var papers =([['Year', 'Number of papers'],['2017',1],['2018',2],['2019',0],['2020',1],]);var citations =([['Year', 'Number of Citations'],['2017',0],['2018',249],['2019',0],['2020',8],]);
