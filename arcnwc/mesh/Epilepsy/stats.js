c0186dd7c913a50f44057f50183feb3071d1befd
Azure pipeline <you@example.com>
2025-11-30 01:36:54 +0000

    Automatically updating web files for arcnwc

--- arcnwc/mesh/Epilepsy/stats.js
+++ arcnwc/mesh/Epilepsy/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2021',1],['2022',2],['2023',2],['2024',6],]);var citations =([['Year', 'Number of Citations'],['2021',3],['2022',14],['2023',14],['2024',32],]);
+var papers =([['Year', 'Number of papers'],['2021',1],['2022',2],['2023',2],['2024',6],['2025',1],]);var citations =([['Year', 'Number of Citations'],['2021',3],['2022',14],['2023',14],['2024',32],['2025',0],]);
