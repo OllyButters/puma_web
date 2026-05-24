6d7fa8b35ee986edb2da52ced44f6e7700fd6736
Azure pipeline <you@example.com>
2026-05-24 01:43:17 +0000

    Automatically updating web files for arcnwc

--- arcnwc/keywords/Staff well-being/stats.js
+++ arcnwc/keywords/Staff well-being/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2022',1],]);var citations =([['Year', 'Number of Citations'],['2022',39],]);
+var papers =([['Year', 'Number of papers'],['2022',1],]);var citations =([['Year', 'Number of Citations'],['2022',40],]);
