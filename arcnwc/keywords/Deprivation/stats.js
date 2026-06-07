bbadf7d0ad1d83fcf3dff019e21e6782e835b5d4
Azure pipeline <you@example.com>
2026-06-07 01:40:38 +0000

    Automatically updating web files for arcnwc

--- arcnwc/keywords/Deprivation/stats.js
+++ arcnwc/keywords/Deprivation/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2018',1],['2019',1],['2020',2],['2021',2],]);var citations =([['Year', 'Number of Citations'],['2018',8],['2019',28],['2020',40],['2021',34],]);
+var papers =([['Year', 'Number of papers'],['2018',1],['2019',1],['2020',2],['2021',2],]);var citations =([['Year', 'Number of Citations'],['2018',8],['2019',28],['2020',40],['2021',35],]);
